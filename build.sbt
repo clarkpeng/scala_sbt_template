f3773d6d8b661cf751a847efcbc35f2f4533e57d
Clark_Peng <Clark_Peng@htc.com>
2015-08-08 21:19:31 +0800

    my first scala template

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "my-project-name"
+
+version := "0.0.1"
+
+scalaVersion := "2.11.4"
+
---
+++ src/main/scala/MyObject.scala
@@ -0,0 +1,11 @@
+import scala.io.Source
+
+object MyObject {
+
+  def main(args: Array[String]) {
+
+    // val data:String = Source.fromInputStream(getClass.getResourceAsStream("intput_file"))("UTF-8").mkString.stripLineEnd
+
+    println("hello world")
+  }
+}
